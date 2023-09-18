9a2b6094697de04a266082f62f65a1a651be1fa2
Vahagn Movsisyan <movsisyanvahagn029@gmail.com>
2023-09-19 00:02:07 +0400

    Homework 5 DynamicArray and Necessary watch DynamicArrayTest

--- src/Test.java
+++ src/Test.java
@@ -1,18 +1,5 @@
-import java.util.Stack;
-
 public class Test {
     public static void main(String[] args) {
-        String str = "{bo(lo){la}]";
 
-        for (int i = 0; i < str.length(); i++) {
-           char examTxt = str.charAt(i);
-
-           switch (examTxt){
-               case '{':
-               case '(':
-               case '[':
-
-           }
-        }
     }
 }
