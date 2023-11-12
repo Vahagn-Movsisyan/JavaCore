5c389f10dd7e9e8e481fc2c10abd3e27089b88b8
Vahagn Movsisyan <movsisyanvahagn029@gmail.com>
2023-11-12 19:10:07 +0400

    Homework 12 practice work with file system by recursion

--- src/homeworks/hm12FileUtil/FileUtil.java
+++ src/homeworks/hm12FileUtil/FileUtil.java
@@ -40,20 +40,25 @@ public class FileUtil implements Command {
 
         File folder = new File(pathAndFilename[0].trim());
         String targetFileName = pathAndFilename[1].trim();
+        fileSearchRecursive(folder, targetFileName);
+    }
 
-        if (folder.exists() && folder.isDirectory()) {
-            File[] files = folder.listFiles();
-
+    static void fileSearchRecursive(File path, String targetFileName) {
+        if (path.exists() && path.isDirectory()) {
+            File[] files = path.listFiles();
             if (files != null) {
                 for (File file : files) {
                     if (file.isFile() && file.getName().equals(targetFileName)) {
-                        System.out.println("File exists");
+                        System.out.println("File is exist");
+                        return;
+                    } else if (file.isDirectory()) {
+                        fileSearchRecursive(file, targetFileName);
                         return;
                     }
                 }
             }
         }
-        System.out.println("File doesn't exist");
+        System.out.println("File dosen't exist");
     }
 
     //այս մեթոդը պետք է սքաններով վերցնի երկու string.
@@ -62,7 +67,6 @@ public class FileUtil implements Command {
     // Մեթոդը պետք է նշված path-ում գտնի բոլոր .txt ֆայլերը, և իրենց մեջ փնտրի
     // մեր տված keyword-ը, եթե գտնի, պետք է տպի տվյալ ֆայլի անունը։
     static void contentSearch() {
-        boolean isFound = false;
         System.out.println("Enter filepath and keyword by ',':");
         String path = SCANNER.nextLine();
         String[] filepathAndKeyword = path.split(",");
@@ -74,18 +78,25 @@ public class FileUtil implements Command {
         }
 
         File folder = new File(filepathAndKeyword[0].trim());
+        String totalKeyword = filepathAndKeyword[1].trim();
 
-        if (folder.exists() && folder.isDirectory()) {
-            File[] files = folder.listFiles();
-            if (files != null) {
-                for (File file : files) {
-                    if (file.getName().endsWith(".txt")) {
+        contentSearchRecursive(folder, totalKeyword);
+    }
+
+    static void contentSearchRecursive(File path, String totalKeyword) {
+        if (path.exists() && path.isDirectory()) {
+            File[] files = path.listFiles();
+            for (File file : files) {
+                if (file != null) {
+                    if (file.isDirectory()) {
+                        contentSearchRecursive(file, totalKeyword);
+                    } else if (file.isFile() && file.getName().endsWith(".txt")) {
                         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
                             String line;
                             while ((line = bufferedReader.readLine()) != null) {
-                                if (line.equals(filepathAndKeyword[1].trim())) {
-                                    System.out.println(file.getName());
-                                    isFound = true;
+                                if (line.contains(totalKeyword)) {
+                                    System.out.println("Found in " + file.getName());
+                                    break;
                                 }
                             }
                         } catch (IOException e) {
@@ -94,99 +105,107 @@ public class FileUtil implements Command {
                     }
                 }
             }
-            if (!isFound) {
-                System.out.println(filepathAndKeyword[1] + " dosen't found!");
-            }
         }
     }
 
-    //այս մեթոդը պետք է սքաններով վերցնի երկու string.
-    // 1 - txtPath txt ֆայլի փաթը
-    // 2 - keyword - ինչ որ բառ
-    // տալու ենք txt ֆայլի տեղը, ու ինչ որ բառ, ինքը տպելու է էն տողերը, որտեղ գտնի էդ բառը։
-    static void findLines() {
-        boolean isFound = false;
-        System.out.println("Enter the path and keyword by ',':");
-        String path = SCANNER.nextLine();
-        String[] patAndKeywordArr = path.split(",");
-        File file = new File(patAndKeywordArr[0].trim());
+        //այս մեթոդը պետք է սքաններով վերցնի երկու string.
+        // 1 - txtPath txt ֆայլի փաթը
+        // 2 - keyword - ինչ որ բառ
+        // տալու ենք txt ֆայլի տեղը, ու ինչ որ բառ, ինքը տպելու է էն տողերը, որտեղ գտնի էդ բառը։
+        static void findLines () {
+            boolean isFound = false;
+            System.out.println("Enter the path and keyword by ',':");
+            String path = SCANNER.nextLine();
+            String[] patAndKeywordArr = path.split(",");
+            File file = new File(patAndKeywordArr[0].trim());
 
-        //To avoid the ArrayIndexOutOfBoundException
-        if (patAndKeywordArr.length != 2) {
-            System.out.println("Invalid input!");
-            return;
+            //To avoid the ArrayIndexOutOfBoundException
+            if (patAndKeywordArr.length != 2) {
+                System.out.println("Invalid input!");
+                return;
+            }
+
+            if (file.exists() && file.isFile()) {
+                try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
+                    String foundLine;
+                    int lineCount = 0;
+
+                    while ((foundLine = bufferedReader.readLine()) != null) {
+                        lineCount++;
+                        if (foundLine.equals(patAndKeywordArr[1].trim())) {
+                            System.out.println(lineCount + "-" + foundLine);
+                            isFound = true;
+                        }
+                    }
+                } catch (IOException e) {
+                    System.out.println(e.getMessage());
+                }
+                if (!isFound) {
+                    System.out.println(patAndKeywordArr[1] + " dosen't found");
+                }
+            }
         }
 
-        if (file.exists() && file.isFile()) {
-            try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
-                String foundLine;
-                int lineCount = 0;
+        //այս մեթոդը պետք է սքաններով վերցնի մեկ string.
+        // 1 - path թե որ ֆոլդերի չափն ենք ուզում հաշվել
+        // ֆոլդերի բոլոր ֆայլերի չափսերը գումարում ենք իրար, ու տպում
+        static void printSizeOfPackage () {
+            System.out.println("Enter the path:");
+            String path = SCANNER.nextLine();
 
-                while ((foundLine = bufferedReader.readLine()) != null) {
-                    lineCount++;
-                    if (foundLine.equals(patAndKeywordArr[1].trim())) {
-                        System.out.println(lineCount + "-" + foundLine);
-                        isFound = true;
+            File folder = new File(path);
+            long totalSize = calculateSizeOfPackage(folder);
+
+            if (totalSize > 0) {
+                System.out.println("Size of the package is " + totalSize);
+            } else {
+                System.out.println("Folder not found or empty");
+            }
+        }
+
+        static long calculateSizeOfPackage (File path){
+            if (path.exists() && path.isDirectory()) {
+                long totalSize = 0;
+                File[] files = path.listFiles();
+                if (files != null) {
+                    for (File file : files) {
+                        if (file.isFile()) {
+                            totalSize += file.length();
+                        } else if (file.isDirectory()) {
+                            totalSize += calculateSizeOfPackage(file);
+                        }
                     }
                 }
-            } catch (IOException e) {
-                System.out.println(e.getMessage());
+                return totalSize;
             }
-            if (!isFound) {
-                System.out.println(patAndKeywordArr[1] + " dosen't found");
-            }
+            return 0;
         }
-    }
 
-    //այս մեթոդը պետք է սքաններով վերցնի մեկ string.
-    // 1 - path թե որ ֆոլդերի չափն ենք ուզում հաշվել
-    // ֆոլդերի բոլոր ֆայլերի չափսերը գումարում ենք իրար, ու տպում
-    static void printSizeOfPackage() {
-        System.out.println("Enter the path:");
-        String path = SCANNER.nextLine();
+        //այս մեթոդը պետք է սքաններով վերցնի երեք string.
+        // 1 - path պապկի տեղը, թե որտեղ է սարքելու նոր ֆայլը
+        // 2 - fileName ֆայլի անունը, թե ինչ անունով ֆայլ է սարքելու
+        // 3 - content ֆայլի պարունակությունը։ Այսինքն ստեղծված ֆայլի մեջ ինչ է գրելու
+        // որպես արդյունք պապկի մեջ սարքելու է նոր ֆայլ, իրա մեջ էլ լինելու է content-ով տվածը
+        static void createFileWithContent () {
+            System.out.println("Enter the path, filename and content by ',':");
+            String pathFilenameContent = SCANNER.nextLine();
+            String[] pathFilenameContentArr = pathFilenameContent.split(",");
 
-        File folder = new File(path);
-        if (folder.exists() && folder.isDirectory()) {
-            File[] files = folder.listFiles();
-            if (files != null) {
-                int filesSize = 0;
-                for (File toCountFileSize : files) {
-                    filesSize += (int) toCountFileSize.length();
+            if (pathFilenameContentArr.length != 3) {
+                System.out.println("Invalid input. You must provide path, filename, and content.");
+                return;
+            }
+
+            File folder = new File(pathFilenameContentArr[0].trim());
+            File newFile = new File(folder, pathFilenameContentArr[1].trim());
+
+            if (folder.exists() && folder.isDirectory()) {
+                try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(newFile))) {
+                    folder.createNewFile();
+                    bufferedWriter.write(pathFilenameContentArr[2].trim());
+                } catch (IOException e) {
+                    System.out.println(e.getMessage());
                 }
-                System.out.println(filesSize);
-            } else {
-                System.out.println("File dosen't found");
-            }
-        } else {
-            System.out.println("File dosen't found");
-        }
-    }
-
-    //այս մեթոդը պետք է սքաններով վերցնի երեք string.
-    // 1 - path պապկի տեղը, թե որտեղ է սարքելու նոր ֆայլը
-    // 2 - fileName ֆայլի անունը, թե ինչ անունով ֆայլ է սարքելու
-    // 3 - content ֆայլի պարունակությունը։ Այսինքն ստեղծված ֆայլի մեջ ինչ է գրելու
-    // որպես արդյունք պապկի մեջ սարքելու է նոր ֆայլ, իրա մեջ էլ լինելու է content-ով տվածը
-    static void createFileWithContent() {
-        System.out.println("Enter the path, filename and content by ',':");
-        String pathFilenameContent = SCANNER.nextLine();
-        String[] pathFilenameContentArr = pathFilenameContent.split(",");
-
-        if (pathFilenameContentArr.length != 3) {
-            System.out.println("Invalid input. You must provide path, filename, and content.");
-            return;
-        }
-
-        File folder = new File(pathFilenameContentArr[0].trim());
-        File newFile = new File(folder, pathFilenameContentArr[1].trim());
-
-        if (folder.exists() && folder.isDirectory()) {
-            try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(newFile))) {
-                folder.createNewFile();
-                bufferedWriter.write(pathFilenameContentArr[2].trim());
-            } catch (IOException e) {
-                System.out.println(e.getMessage());
             }
         }
     }
-}
